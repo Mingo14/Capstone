2b5e01daf3ec49a02a2e6fa41847d62d820c20a9
maddy b <mbradfield@student.neumont.edu>
2020-01-14 07:16:53 -0700

    initial commit

---
+++ Site.go
@@ -0,0 +1,19 @@
+package main
+import (
+	"fmt"
+	// "io/ioutil"
+)
+
+func main() {
+	fmt.Println("hllo")
+}
+
+// type Page struct {
+// 	Title string
+// 	Body []byte
+// }
+
+// func (p *Page) save() error {
+// 	filename := p.Title + ".txt"
+// 	return ioutil.Write
+// }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Mingo14/Capstone.git
+
+go 1.13
