f3f8393b98d5db0997c0bb760bf67dbe782b5b8b
maddy b <mbradfield@student.neumont.edu>
2020-01-18 12:15:22 -0700

    broken can request but printing data no work

--- Site.go
+++ Site.go
@@ -3,9 +3,14 @@ package main
 import (
     "fmt"
     "net/http"
+    "log"
+    "encoding/json"
+    "net/url"
+    // "bytes"
 )
 
 func main() {
+    MakeRequest() 
     http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Welcome to my website!")
     })
@@ -14,4 +19,24 @@ func main() {
     http.Handle("/static/", http.StripPrefix("/static/", fs))
 
     http.ListenAndServe(":8080", nil)
+
 }
+
+func MakeRequest() {
+
+	formData := url.Values{
+		"name": {"masnun"},
+	}
+
+	resp, err := http.PostForm("https://api.themoviedb.org/3/movie/now_playing?api_key=8899bc310c15c2755e2703aed0345bc5", formData)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var result map[string]interface{}
+
+	json.NewDecoder(resp.Body).Decode(&result)
+
+	log.Println(result["form"])
+}
+// "https://api.themoviedb.org/3/movie/now_playing?api_key=8899bc310c15c2755e2703aed0345bc5"
